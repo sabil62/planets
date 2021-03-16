49a40fd10b7152a46cd8468594cd460e4711d69f
sabil62 <shresthasabil62@gmail.com>
2021-03-16 13:17:46 +0545

    created planetInfo class and swipper to display all the planet card deck

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:planet/constants.dart';
+import 'package:planet/data.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -45,7 +47,85 @@ class _HomePageState extends State<HomePage> {
                   onChanged: (value) {},
                   icon: Image.asset("assets/drop_down_icon.png"),
                   underline: SizedBox(),
-                )
+                ),
+                Container(
+                    height: 500,
+                    child: Swiper(
+                      itemCount: planets.length,
+                      itemWidth: MediaQuery.of(context).size.width - 2 * 64,
+                      layout: SwiperLayout.STACK,
+                      itemBuilder: (BuildContext context, index) {
+                        return Stack(
+                          children: [
+                            Column(
+                              children: [
+                                SizedBox(
+                                  height: 100,
+                                ),
+                                Card(
+                                  color: Colors.white,
+                                  elevation: 8,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(32)),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(32.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        SizedBox(
+                                          height: 100,
+                                        ),
+                                        Text(
+                                          planets[index].name,
+                                          style: TextStyle(
+                                              fontFamily: "Avenir",
+                                              fontSize: 36,
+                                              fontWeight: FontWeight.w900,
+                                              color: Color(0xff47455f)),
+                                          textAlign: TextAlign.left,
+                                        ),
+                                        SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text(
+                                          "Solar System",
+                                          style: TextStyle(
+                                              fontFamily: "Avenir",
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.w500,
+                                              color: primaryTextColor),
+                                        ),
+                                        SizedBox(
+                                          height: 28,
+                                        ),
+                                        Row(
+                                          children: [
+                                            Text(
+                                              "Know More",
+                                              style: TextStyle(
+                                                  fontFamily: "Avenir",
+                                                  fontSize: 16,
+                                                  fontWeight: FontWeight.w500,
+                                                  color: secondaryTextColor),
+                                              textAlign: TextAlign.left,
+                                            ),
+                                            Icon(
+                                              Icons.arrow_forward,
+                                              color: secondaryTextColor,
+                                            )
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              ],
+                            )
+                          ],
+                        );
+                      },
+                    )),
               ],
             ),
           ),
